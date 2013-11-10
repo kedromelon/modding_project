8a29c2dd11ff209c423b57d665ff1525d7e49a2a
Will Jeffers <wfj6193@comcast.net>
2013-11-10 14:44:21 -0500

    Added second player. Added playerkick scripts for both players so they bounce off each other.

---
+++ Assets/Scripts/player2kick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player2kick: MonoBehaviour {
+	
+	public float hitForce = 2f;
+	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.name == "player 1" && rigidbody.velocity.magnitude > collision.rigidbody.velocity.magnitude){
+			collision.rigidbody.velocity = Vector3.zero;
+			collision.rigidbody.AddForce(rigidbody.velocity * hitForce);
+			rigidbody.velocity = Vector3.zero;
+		}
+	}
+}
+	
