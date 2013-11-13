f127823298a888c8dca5a31e483253cc2d4e7794
Noah Lemen <noah.lemen@gmail.com>
2013-11-12 19:49:04 -0500

    switched to capsule colliders

---
+++ Assets/Scripts/UpdateInputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class UpdateInputManager : MonoBehaviour {
+
+	void Start()
+	{
+		InputManager.Setup();
+	}
+	
+	void Update()
+	{
+		InputManager.Update();
+	}
+
+}
