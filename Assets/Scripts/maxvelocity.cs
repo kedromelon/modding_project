c7debf905548dbebbd2ae2a165d69f5e0cc9fcf5
Noah Lemen <noah.lemen@gmail.com>
2013-11-14 10:35:26 -0500

    created maxvelocity script, applied to player and ball prefabs; added drag on ball

---
+++ Assets/Scripts/maxvelocity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class maxvelocity: MonoBehaviour {
+
+	public float limit = 300f;
+
+	void FixedUpdate () {
+	
+		Vector3 currentVelocity = rigidbody.velocity;
+
+		if (currentVelocity.sqrMagnitude > limit*limit){
+
+			currentVelocity = currentVelocity.normalized * limit;
+			
+			rigidbody.velocity = currentVelocity;
+
+		}
+
+	}
+}
