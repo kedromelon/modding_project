4e135798443e9156f8c14e5e83002fa4d9e5d5e4
Noah Lemen <noah.lemen@gmail.com>
2013-11-16 13:09:38 -0500

    updated playerkick to use layers for detecting collision with players

--- Assets/Scripts/New Player Scripts/kickplayer.cs
+++ Assets/Scripts/New Player Scripts/kickplayer.cs
@@ -6,10 +6,11 @@ public class kickplayer: MonoBehaviour {
 	public float hitForce = 150f;
 	
 	void OnCollisionEnter(Collision collision) {
-		if (collision.gameObject.GetComponent<player>() && rigidbody.velocity.magnitude > collision.rigidbody.velocity.magnitude){
+		if (collision.gameObject.layer == 8 && rigidbody.velocity.magnitude > collision.rigidbody.velocity.magnitude){
 			collision.rigidbody.velocity = Vector3.zero;
 			collision.rigidbody.AddForce(rigidbody.velocity * hitForce);
 			rigidbody.velocity = Vector3.zero;
+			Debug.Log("HIT");
 		}
 	}
 }
