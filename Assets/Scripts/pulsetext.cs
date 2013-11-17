f90b797928cb93f710a996540957add32bb2d204
Noah Lemen <noah.lemen@gmail.com>
2013-11-17 15:09:47 -0500

    added pulsetext script

---
+++ Assets/Scripts/pulsetext.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pulsetext : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<TextMesh>().color = new Color( 1f, 1f, 1f,(Mathf.Cos(Time.time * 4f)+1)/2);
+	}
+}
