369ae455829b578a58cab0029d92e0e383feecdb
Matt Chen <matt.chen312@gmail.com>
2013-11-19 12:16:54 -0500

    Moved some scripts into proper folders and created the goal ring + look at script + started writing the scoring script.

---
+++ Assets/Scripts/Level Scripts/SCORE.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCORE : MonoBehaviour {
+
+    public noahballkick obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Scoring
+    void OnTriggerEnter()
+    {
+        
+    }
+
+}
