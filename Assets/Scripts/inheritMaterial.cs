46e66f5a3693051b50ff8b6d836f904c0e216c67
Noah Lemen <noah.lemen@gmail.com>
2013-12-11 21:27:36 -0500

    hexagonal goal added

---
+++ Assets/Scripts/inheritMaterial.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inheritMaterial : MonoBehaviour {
+
+	Transform parentTransform;
+
+	void Start () {
+		parentTransform = transform.parent;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.renderer.material = parentTransform.renderer.material;
+	}
+}
